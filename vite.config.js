339e001e09f67d07bccb0ef7964126ceba228d03
luchv1 <havietlucit2000@gmail.com>
2025-08-14 10:27:39 +0700

    feat: apply pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Poro Focus',
+        short_name: 'Poro Focus',
+        display: 'standalone',
+        icons: [
+          { src: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+        ]
+      }
+    })
+  ],
 })
